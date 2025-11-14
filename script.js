84baf97544195cfc4b08079b186389c5448c9c3c
yijui0613-glitch <yijui0613@gmail.com>
2025-11-14 22:41:40 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+function searchId(){alert("ID saved: " + document.getElementById('tiktokId').value);}
+function showSuccess(){document.getElementById('successModal').style.display='flex';}
+function closeModal(){document.getElementById('successModal').style.display='none';}
