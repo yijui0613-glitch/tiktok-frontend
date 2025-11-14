17ac6131596a984ddd42aa114bb2a984c9d2bb91
yijui0613-glitch <yijui0613@gmail.com>
2025-11-14 22:49:09 +0800

    Delete script.js

--- script.js
+++
@@ -1,3 +0,0 @@
-function searchId(){alert("ID saved: " + document.getElementById('tiktokId').value);}
-function showSuccess(){document.getElementById('successModal').style.display='flex';}
-function closeModal(){document.getElementById('successModal').style.display='none';}
